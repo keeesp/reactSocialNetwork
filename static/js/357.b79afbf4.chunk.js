714bd88753681e6982d17dcd3c307e28270a3f59
kirill shubrat <kirill.shubrat@mail.ru>
2023-08-20 19:40:04 +0300

    Updates

---
+++ static/js/357.b79afbf4.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_app=self.webpackChunkmy_app||[]).push([[357],{2357:function(e,n,t){t.r(n);var u=t(184);n.default=function(e){return(0,u.jsx)("div",{children:"Settings"})}}}]);
+//# sourceMappingURL=357.b79afbf4.chunk.js.map
